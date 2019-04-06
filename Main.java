bb0f4b3f862164f09f77fd55cb004e05606cc7ab
fViktor86 <f.viktor1986@gmail.com>
2019-04-06 11:52:04 +0200

    16. feladat

--- Main.java
+++ Main.java
@@ -10,28 +10,33 @@ public class Main {
         System.out.println(Arrays.toString(tomb_10_0_100));
         System.out.println();
 
+        System.out.println("1. feladat");
         System.out.println("Első öt szám:");
         for (int i = 0; i < 5; i++) {
             System.out.println(tomb_10_0_100[i]);
         }
         System.out.println();
 
+        System.out.println("2. feladat");
         System.out.println("Utolsó öt szám:");
         for (int i = 0; i < 5; i++) {
             System.out.println(tomb_10_0_100[i+5]);
         }
         System.out.println();
 
+        System.out.println("3. feladat");
         int tmpOsszeg = tomb_10_0_100[0] + tomb_10_0_100[tomb_10_0_100.length-1];
         System.out.println("Első és utolsó összege: " + tmpOsszeg);
         System.out.println();
 
+        System.out.println("4. feladat");
         System.out.println("Fordított sorrendben:");
         for (int i = tomb_10_0_100.length-1; i >= 0; i--){
             System.out.println(tomb_10_0_100[i]);
         }
         System.out.println();
 
+        System.out.println("5. feladat");
         int tmpDb = 0;
         for (int i = 0; i < tomb_10_0_100.length; i++){
             if (i < (tomb_10_0_100.length-1) && tomb_10_0_100[i] < tomb_10_0_100[i+1]){
@@ -41,6 +46,7 @@ public class Main {
         System.out.println(tmpDb + " darab olyan elem van amit egy tőle nagyobb követ.");
         System.out.println();
 
+        System.out.println("6. feladat");
         int[] tomb_10_0_20 = tombGeneralas.tombVissza(10, 0, 20);
         System.out.println("Tömb: 10 elemű, 0-20:");
         System.out.println(Arrays.toString(tomb_10_0_20));
@@ -57,6 +63,7 @@ public class Main {
             System.out.println();
         }
 
+        System.out.println("7. feladat");
         int[] tomb_20_0_10 = tombGeneralas.tombVissza(20, 0, 10);
         System.out.println("Tömb: 20 elemű, 0-10:");
         System.out.println(Arrays.toString(tomb_20_0_10));
@@ -73,6 +80,7 @@ public class Main {
             System.out.println();
         }
 
+        System.out.println("8. feladat");
         for (int i = 0; i < tomb_10_0_20.length; i++) {
             if (i < (tomb_10_0_20.length-1) && (tomb_10_0_20[i]*2) == tomb_10_0_20[i+1]){
                 System.out.println("A " + (i+1) + "-ik elem (" + tomb_10_0_20[i+1] + ") kétszerese az előte következőnek (" + tomb_10_0_20[i] + ")");
@@ -80,6 +88,7 @@ public class Main {
         }
         System.out.println();
 
+        System.out.println("9. feladat");
         int[] tomb_5_0_1000 = tombGeneralas.tombVissza(5, 0, 1000);
         System.out.println("Tömb: 5 elemű, 0-1000:");
         System.out.println(Arrays.toString(tomb_5_0_1000));
@@ -91,11 +100,10 @@ public class Main {
                 novekvo = false;
             }
         }
-        if (!novekvo) {
-            System.out.println("A tömb nem növekvő sorrendbe generálódott.");
-            System.out.println();
-        }
+        System.out.println(novekvo ? "A tömb növekvő sorrendbe generálódott." : "A tömb nem növekvő sorrendbe generálódott.");
+        System.out.println();
 
+        System.out.println("10. feladat");
         for (int i = 0; i < tomb_10_0_20.length; i++) {
             if (i < tomb_10_0_20.length-2 && tomb_10_0_20[i+1] == (tomb_10_0_20[i] + tomb_10_0_20[i+2])){
                 System.out.println("A " + (i+1) + ".-ik (" + tomb_10_0_20[i+1] + ") elem a szomszédai összegével egyenlő. (" + tomb_10_0_20[i] + " és " + tomb_10_0_20[i+2] + ")");
@@ -103,6 +111,7 @@ public class Main {
         }
         System.out.println();
 
+        System.out.println("11. feladat és 14. feladat");
         int[] tombOt_0_5 = new int[5];
         System.out.println("5 elemű: 0-5");
         for (int i = 0; i < tombOt_0_5.length; i++) {
@@ -124,6 +133,7 @@ public class Main {
         System.out.println(egyenloek ? "A tömb előlről és hátulról is ugyanaz." : "A tömb előlről és hátulról nem ugyanaz.");
         System.out.println();
 
+        System.out.println("12. feladat");
         int segedSzam = (int)(Math.random()*5+1);
         System.out.print("SegédSzám: " + segedSzam + " | Tömb: " + Arrays.toString(tomb_10_0_20) + " | Kiválogatás: { ");
         for (int i = 0; i < tomb_10_0_20.length; i++) {
@@ -134,6 +144,7 @@ public class Main {
         System.out.print("}");
         System.out.println();
 
+        System.out.println("13. feladat");
         System.out.print("SegédSzám: " + segedSzam + " | Tömb: " + Arrays.toString(tomb_10_0_20) + " | Kiválogatás: { ");
         for (int i = 0; i < segedSzam; i++) {
             System.out.print(tomb_10_0_20[i] + " ");
@@ -145,6 +156,7 @@ public class Main {
         System.out.println();
         System.out.println();
 
+        System.out.println("15. feladat");
         segedSzam = (int)(Math.random()*4+4);
         int[] tomb_random_0_20 = tombGeneralas.tombVissza(segedSzam, 0, 20);
         int eleje = 0, vege, cMag = 0;
@@ -182,5 +194,22 @@ public class Main {
             System.out.print("nem");
         }
         System.out.println();
+        System.out.println();
+
+        System.out.println("16. feladat");
+        int[] tomb_10__30_100 = new int[10];
+        int tmpErtek = 0;
+        for (int i = 0; i < tomb_10__30_100.length; i++) {
+            do{
+                tmpErtek = (int)(Math.random()*131-30);
+            }
+            while (tmpErtek % 2 == 0);
+            tomb_10__30_100[i] = tmpErtek;
+        }
+        System.out.println(Arrays.toString(tomb_10__30_100));
+        System.out.println();
+
+        System.out.println("17. feladat");
+
     }
 }
